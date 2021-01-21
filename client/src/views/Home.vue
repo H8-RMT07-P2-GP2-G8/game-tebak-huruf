a7704dd3cd1f0c369a4943899e38aac3ea1cdd25
janakraman <hifzhana@gmail.com>
2021-01-22 02:49:54 +0700

    added start button

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <form @submit.prevent="join">
+      <b-container class="mt-5">
+        <b-row>
+          <b-col md="12">
+            <b-card title="Masukkan nama anda" class="text-center border-light shadow">
+              <b-form>
+                <b-input v-model="name" placeholder="type..."></b-input>
+                <b-button @click.prevent="join" type="submit" variant="primary" class="mt-3">Submit</b-button>
+              </b-form>
+            </b-card>
+          </b-col>
+      </b-row>
+    </b-container>
+    <!-- <form @submit.prevent="join">
       <input
         type="text"
         id="username"
         v-model="name"
         placeholder="name"
       >
-      <button type="submit" value="join">join</button> <!-- user klik join -->
-    </form>
+      <button type="submit" value="join">join</button>  user klik join -->
+    <!-- </form> -->
   </div>
 </template>
 
