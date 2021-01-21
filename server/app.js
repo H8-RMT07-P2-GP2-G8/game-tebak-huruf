f3da341b7615875a9afc55170db662f82feab74b
janakraman <hifzhana@gmail.com>
2021-01-21 19:40:51 +0700

    fixed server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const app = require('express')();
 const server = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(server);
 const PORT = 3000
 
 const players = []
@@ -38,6 +38,6 @@ io.on('connection', (socket) => {
   })
 });
 
-http.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
 });
