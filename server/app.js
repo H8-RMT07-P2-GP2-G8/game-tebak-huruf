4bc7a0d5166e2f70ebae7e596949469b9f90de72
janakraman <hifzhana@gmail.com>
2021-01-22 14:04:40 +0700

    flexible port

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 let players = []
 let started = false
